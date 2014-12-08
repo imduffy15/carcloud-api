1d3f95725f4502a2431df1ee1bc91ffb3762819a
Ian Duffy <ian@ianduffy.ie>
2014-12-08 01:55:30 +0000

    general clean up

---
+++ src/main/resources/config/liquibase/liquibase-dump.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+set -x -e
+
+DB="carcloud"
+DRIVER_VERSION=5.1.34
+
+function prep_driver_jar(){
+    if [ -n "$DRIVER" ]; then
+        echo $DRIVER
+    else
+        if [ -z "$(find $HOME/.m2 -name mysql-connector-java*.jar)" ]; then
+            mvn -DgroupId=mysql -DartifactId=mysql-connector-java -Dversion=$DRIVER_VERSION dependency:get &> /dev/null
+        fi
+        if [ -n "$(find $HOME/.m2 -name mysql-connector-java*.jar)" ]; then
+            echo $(find $HOME/.m2 -name mysql-connector-java*.jar -print -quit)
+        else
+            # Couldn't install driver
+            return 1
+        fi
+    fi
+}
+
+if [ -e dump.xml ]; then
+    mv dump.xml dump-$(date '+%s').xml
+fi
+
+DRIVER_JAR=$(prep_driver_jar)
+
+liquibase --classpath="$DRIVER_JAR"  \
+    --driver=com.mysql.jdbc.Driver \
+    --changeLogFile=dump.xml \
+    --url="jdbc:mysql://localhost:3306/${DB}" \
+    --username=root generateChangeLog
+
+sed -i -E \
+    -e '2r header.xml' \
+    -e 's/id="[0-9]+-([0-9]+)/id="dump\1/g' \
+    -e 's/BIGINT\(19\)/BIGINT/g' \
+    -e 's/BIT\(1\)/BIT/g' \
+    -e 's/INT\(10\)/INT/g' \
+    dump.xml
+
+cat dump.xml
