7e0b6f6e51a595d1d3ed4056e117cd68b74bcfa5
Ian Duffy <ian@ianduffy.ie>
2015-01-28 15:01:18 +0000

    add field boolean

--- src/main/java/ie/ianduffy/carcloud/domain/FieldBoolean.java
+++ src/main/java/ie/ianduffy/carcloud/domain/FieldBoolean.java
@@ -1,23 +1,29 @@
-//package ie.ianduffy.carcloud.domain;
-//
-//import org.hibernate.annotations.Cache;
-//import org.hibernate.annotations.CacheConcurrencyStrategy;
-//
-//import javax.persistence.Entity;
-//import javax.persistence.Inheritance;
-//import javax.persistence.InheritanceType;
-//import javax.persistence.Table;
-//
-//@Entity
-//@Table(name = "T_FIELD_BOOLEAN")
-//@Cache(usage = CacheConcurrencyStrategy.NONSTRICT_READ_WRITE)
-//public class FieldBoolean extends Field<Boolean> {
-//
-//
-//    public FieldBoolean() {
-//    }
-//
-//    public FieldBoolean(String name, Boolean value) {
-//        super(name, value);
-//    }
-//}
+package ie.ianduffy.carcloud.domain;
+
+import lombok.Data;
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+
+import javax.persistence.Entity;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.Table;
+
+@Data
+@Entity
+@Table(name = "T_FIELD_BOOLEAN")
+@Cache(usage = CacheConcurrencyStrategy.NONSTRICT_READ_WRITE)
+public class FieldBoolean extends Field<Boolean> {
+
+
+    private Boolean value;
+
+    public FieldBoolean() {
+
+    }
+
+    public FieldBoolean(String name, Boolean value) {
+        super(name);
+        this.value = value;
+    }
+}
